b23e5792dd62cbc7c162501738e1f44b38fa95be
Brian Hans <wbrianw8@gmail.com>
2016-10-12 17:00:09 -0700

    Finished challenge 5

--- Flask/server.py
+++ Flask/server.py
@@ -10,20 +10,26 @@ petList = []
 def hello():
     return 'hello'
 
-@app.route('/pets', methods=['POST', 'GET'])
+@app.route('/pets/', methods=['POST', 'GET'])
 def pets():
     if(request.method == 'POST'):
         json = request.get_json()
         try:
             if('name' in json and 'age' in json and 'species' in json):
                 petList.append({'name' : json['name'], 'age' : json['age'], 'species' : json['species']})
-
+                return jsonify({'code' : 200})
         except:
             print('error')
             return jsonify({'error' : 'Please include a name, age, and species'})
     elif(request.method == 'GET'):
         return jsonify(petList)
 
+@app.route('/pets/<name>', methods=['GET'])
+def getPet(name):
+    for pet in petList:
+        if(pet['name'] == name):
+            return petList[name]
+    return jsonify({'error' : 'Not in database'}), 404
 
 if __name__ == '__main__':
     app.run()
